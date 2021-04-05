b3730cd5d1b1a94f308bf19037bdbc6f3d631e2b
Rahul Rao <RAOR378647@exactsoftware.com>
2021-04-05 17:12:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //constructor
+  constructor() {
+    super();
+    this.message = 'Good evening!!!'
+  }
+
+  // functions
+  myStyle = {
+    fontSize: 50,
+    color: 'red'
+  };
+
+  getName = function (prefix) {
+    return prefix + '. ' + 'Rahul G Rao';
+  }
+
+  render() {
+    // variables
+    const navBar = <nav>I am a navigation bar</nav>
+    var i = 1;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span>Welcome to React, {this.getName('Mr')}</span>
+          <span>{this.message}</span>
+
+          {/* variable */}
+          {navBar}
+
+          <p style={this.myStyle}>React is awesome</p>
+
+          {/* custom attribute */}
+          <p data-myattribute="somevalue">This is the content!</p>
+
+
+          <p>
+            Sum of these numbers 100 and 200 = {100 + 200}
+          </p>
+
+          {/* ternary operator */}
+          <p>
+            {i === 1 ? "true" : "false"}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
